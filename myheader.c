4aaeaef0acb8397f499fa346837c61db26f5be7e
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-02-03 19:28:47 +0200

    Update myheader.c

--- myheader.c
+++ myheader.c
@@ -1,19 +1,26 @@
 #include <fcntl.h>
 #include <stdlib.h>
-#include <unistd.h> 
+#include <unistd.h>
+#include <stdio.h>
 
 char *get_next_line(int fd)
 {
-    // char static *stash;
-    // char *line;
+    // static char *stash; // Static variable to store leftover characters
+    //  char *line;
 
     if (fd > 0)
     {
-        char buffer[1024];
+        char *buffer = malloc(1024 + 1);
+        if (!buffer)
+        {
+            return NULL; // Handle malloc failure
+        }
 
-        read(fd, buffer, sizeof(buffer));
+        int bytesRead = read(fd, buffer, 1024);
+        buffer[bytesRead] = '\0'; // Null-terminate the string
+        printf("%d bytes read!\n", bytesRead);
+        printf("File Contents: %s\n", buffer);
         return buffer;
-        
     }
     else
     {
