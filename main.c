74a1fdc94ad38ef88fd651e19b6088828fd2760f
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-02-03 19:28:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include "myheader.h"
 #include <fcntl.h>
+#include <stdio.h>  // For printf
+#include <stdlib.h> // For free
+#include <unistd.h> // For close
 
 int main()
 {
 
     // opening file in read/write mode with open function
     int fd = open("test.txt", O_RDWR);
+    if (fd == -1)
+    {
+        printf("error could not open file \n");
+        return 1;
+    }
 
-    get_next_line(fd);
-}
+    get_next_line(fd);  
+}    
+    
