2c358c5b0d9352a154be2f4b2b278244c23ea591
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-02-03 20:36:41 +0200

    Update myheader.c

--- myheader.c
+++ myheader.c
@@ -5,8 +5,8 @@
 
 char *get_next_line(int fd)
 {
-    // static char *stash; // Static variable to store leftover characters
-    //  char *line;
+    static char *stash; // Static variable to store leftover characters
+    char *line;
 
     if (fd > 0)
     {
@@ -16,22 +16,42 @@ char *get_next_line(int fd)
             return NULL; // Handle malloc failure
         }
 
-        int bytesRead = read(fd, buffer, 1024);
-        buffer[bytesRead] = '\0'; // Null-terminate the string
-        printf("%d bytes read!\n", bytesRead);
-        printf("File Contents: %s\n", buffer);
-        return buffer;
-    }
-    else
-    {
-        return NULL;
-    }
-}
+        int bytesRead = 5;
 
-// char *_fill_line_buffer(int fd, char *left_c, char *buffer)
-// {
-// }
+        while (bytesRead > 0)
+        {
+            if (bytesRead <= 0)
+                break; // Break if no more data is read or error occurs
 
-// char *_set_line(char *line_buffer)
-// {
-// }
+            bytesRead = read(fd, buffer, 5);
+            // populate stash
+            for (int i = 0; i < bytesRead; i++)
+            {
+                if (buffer[i] == '\n')
+                {
+                    *stash = '\0'; // null terminating string
+                    // populating the line, moving backwards in stash and forward in line
+                    while (*--stash)
+                    {
+                        *line++ = *stash; // Move characters forward to line
+                    }
+
+                    // clean stash
+                }
+
+                // Append current character to stash
+                *stash++ = buffer[i]; // Move stash pointer forward            }
+            }
+
+            printf("File Contents: %s\n", buffer);
+            return buffer;
+        }
+    }
+
+    // char *_fill_line_buffer(int fd, char *left_c, char *buffer)
+    // {
+    // }
+
+    // char *_set_line(char *line_buffer)
+    // {
+    // }
